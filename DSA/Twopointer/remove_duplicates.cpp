1d50ac55e43de9940ad5e825e6348ad5a70bd315
Aman <amankanujiya66@gmail.com>
2026-02-04 01:41:17 +0530

    Implement cycle detection for duplicate removal
    Refactor duplicate removal logic to use cycle detection.

--- DSA/Twopointer/remove_duplicates.cpp
+++ DSA/Twopointer/remove_duplicates.cpp
@@ -1,31 +1,22 @@
-#include <bits/stdc++.h>
-using namespace std;
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = nums[0];
+        int fast = nums[0];
 
-// Problem: Remove Duplicates from Sorted Array (LeetCode 26)
-// Approach: Two Pointers
-// Time: O(n)
-// Space: O(1)
+        // Phase 1: Detect cycle
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
 
-int main() {
-    vector<int> nums = {0,0,1,1,1,2,2,3,3,4};
-
-    if (nums.size() == 0) return 0;
-
-    int i = 0; // slow pointer
-    for (int j = 1; j < nums.size(); j++) { // fast pointer
-        if (nums[j] != nums[i]) {
-            i++;
-            nums[i] = nums[j];
+        // Phase 2: Find entrance to cycle
+        slow = nums[0];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
         }
+
+        return slow;
     }
-
-    int k = i + 1; // number of unique elements
-
-    cout << "k = " << k << endl;
-    cout << "Array after removing duplicates: ";
-    for (int idx = 0; idx < k; idx++) {
-        cout << nums[idx] << " ";
-    }
-
-    return 0;
-}
+};
